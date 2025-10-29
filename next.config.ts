d3c108d644977654d469135cbd29409a27d2b6b5
satya <satya@pengwintech.com>
2025-10-29 11:52:39 +0530

    added config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/videos/:path*",       // what you call in your frontend
+        destination: "http://yogalandtv.com/vid/:path*", // where to fetch from
+      },
+    ];
+  },
 };
 
 export default nextConfig;
