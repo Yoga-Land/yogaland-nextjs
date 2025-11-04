85a9cf6e3777d0e1d7daed436f83e38e5c6a80de
satya <satya@pengwintech.com>
2025-11-04 14:32:21 +0530

    added view route

---
+++ app/api/videos/[id]/views/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/lib/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PUT(
+  req: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } =  await params;
+
+    const video = await prisma.video.update({
+      where: { id },
+      data: {
+        views: { increment: 1 },
+      },
+    });
+
+    return NextResponse.json(video);
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 400 });
+  }
+}
